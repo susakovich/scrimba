d54e36514e21622042c4010c27207a2fb0d02d0b
Danijel <susakovich@gmail.com>
2023-05-19 12:35:14 +0200

    Add new function to announce winner

--- 8. basketball-scoreboard/index.js
+++ 8. basketball-scoreboard/index.js
@@ -1,38 +1,55 @@
 let homeResult = document.getElementById("home-result");
 let guestResult = document.getElementById("guest-result");
+let winner = document.getElementById("winner");
 
 let homeCount = 0;
 let guestCount = 0;
 
+function updateWinner() {
+  if (homeCount > guestCount) {
+    winner.textContent = `The Home is leading!`;
+  } else if (homeCount < guestCount) {
+    winner.textContent = "The Guest is leading!";
+  } else {
+    winner.textContent = "It's a tie!";
+  }
+}
+
 function addOneHome() {
   homeCount++;
   homeResult.textContent = homeCount;
+  updateWinner();
 }
 
 function addTwoHome() {
   homeCount += 2;
   homeResult.textContent = homeCount;
+  updateWinner();
 }
 
 function addThreeHome() {
   homeCount += 3;
   homeResult.textContent = homeCount;
+  updateWinner();
 }
 
 // Guest counter
 function addOneGuest() {
   guestCount++;
   guestResult.textContent = guestCount;
+  updateWinner();
 }
 
 function addTwoGuest() {
   guestCount += 2;
   guestResult.textContent = guestCount;
+  updateWinner();
 }
 
 function addThreeGuest() {
   guestCount += 3;
   guestResult.textContent = guestCount;
+  updateWinner();
 }
 
 function newGame() {
@@ -40,4 +57,6 @@ function newGame() {
   guestResult.textContent = 0;
   homeCount = 0;
   guestCount = 0;
+  updateWinner();
+  winner.textContent = "Game is starting soon";
 }
