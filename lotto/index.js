58c8848c6884c848b54a8f1a0f093309c88a1f69
Danijel <susakovich@gmail.com>
2023-07-11 10:03:48 +0200

    Finish lotto project

---
+++ lotto/index.js
@@ -0,0 +1,22 @@
+const lottoNums = document.getElementById("lotto-nums");
+
+function getLottoNumbers() {
+  const winningNums = [];
+  for (let i = 0; i < 6; i++) {
+    winningNums.push(Math.round(Math.random() * 100));
+  }
+  return winningNums;
+}
+
+function getWinningNumbersHtml() {
+  return getLottoNumbers()
+    .map(function (num) {
+      return `<div class="number">${num}</div>`;
+    })
+    .join("");
+}
+
+lottoNums.addEventListener("click", function () {
+  document.getElementById("winning-numbers").innerHTML =
+    getWinningNumbersHtml();
+});
