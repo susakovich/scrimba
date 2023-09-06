1771057a5a6faff3957d855dc5ae9b6a347372e4
Danijel <susakovich@gmail.com>
2023-09-06 13:18:14 +0200

    Finish react using cdn

---
+++ REACT/jsxCDN/main.js
@@ -0,0 +1,12 @@
+const navBar = (
+  <nav>
+    <h1>CodeCafe</h1>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navBar, document.getElementById("root"));
