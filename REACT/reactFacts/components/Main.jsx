0359a4d9c84de2dbacd1045474d94b8bb2b6ab96
Danijel <susakovich@gmail.com>
2023-09-08 00:06:05 +0200

    Finish project 1

---
+++ REACT/reactFacts/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul className="fact-list">
+        <li>Was first released in 2013</li>
+        <li>Was orginally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
---
+++ REACT/reactFacts/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img className="logo" src="logo.png" alt="React logo" />
+      <h3 className="logo-name">ReactFacts</h3>
+      <h4 className="react-course">React Course - Project 1</h4>
+    </nav>
+  );
+}
